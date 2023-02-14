86804bac2e194bf810230ebe7c1fa4f4c750049b
amal <amalashraf04@gmail.com>
2023-02-14 13:45:47 +0530

    todo 1.0

---
+++ todo.js
@@ -0,0 +1,94 @@
+//redirect if not valid login
+let currentUser=JSON.parse(window.localStorage.getItem('user'));
+
+if(!currentUser){
+    window.location='index.html';
+
+}
+console.log(currentUser);
+
+$('#welcomeText').html(`Hello ${currentUser.username}! <br> Welcome to your Todo List`);
+
+// code for log out
+$('#logOut').on('click',(e)=>{
+    e.preventDefault();
+    // Sign-out successful.
+    window.localStorage.removeItem('user');
+    window.location="index.html";
+    console.log('logout successfully');
+})
+
+// fetch data.
+const getList=async ()=>{
+    try{
+        const res=await axios.get('https://jsonplaceholder.typicode.com/todos');
+        const lists=res.data;
+        console.log(lists);
+        let listcontent='';
+        lists.forEach((el,index)=>{
+            listcontent+=`<li class="list-group-item ${el.completed?'disabledList':''} ${index%2?'list-group-item-info':'list-group-item-success'}"> <input type="checkbox" class="checkbox" ${el.completed?' checked':''}/> <label for=""> ${el.title}</label></li>`
+        });
+        $('#todoList').html(listcontent);
+        if(checkedCount){
+            checkedCount=0;
+        }
+
+    }
+    catch(e){
+        console.log('failed to fetch lists data',e);
+    }
+}
+// call getlist() when GET LIST is clicked.
+$('#getList').on('click',(e)=>{
+    e.preventDefault();
+    getList();
+});
+
+
+let checkedCount=0;
+
+const alertPromise= ()=>{
+     return new Promise((resolve,reject)=>{
+
+         
+        if(checkedCount===5){
+            resolve(checkedCount)
+        }
+        else{
+            reject('count not equal to 5');
+        }
+    });
+}
+
+const promiseCall=()=>{
+    alertPromise().then((data)=>{
+        alert(`Congrats. 5 Tasks have been Successfully Completed`);
+    })
+    .catch((err)=>{
+        console.log('promise rejected');
+    })
+}
+
+
+getList();
+
+$('#todoList').on('change','.checkbox',function(e){
+    if($(this).prop('checked')===true){
+        console.log('checked');
+        checkedCount++; 
+        $(this).parent().addClass('active');
+    }
+    else{
+        checkedCount--;
+        console.log('unchecked');
+        $(this).parent().removeClass('active');
+    }
+    
+    promiseCall();
+
+
+});
+
+
+
+
---
+++ validation.js
@@ -0,0 +1,20 @@
+function login() {
+    
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "admin" && password === "12345") {
+      redirectToMainPage();
+    } else {
+        alert("Incorrect username or password");
+        window.location='index.html';
+    //   document.getElementById("error-message").innerHTML = "Incorrect username or password";
+    }
+  }
+
+  function redirectToMainPage() {
+    alert("Login Successfully");
+    
+    window.open('todo.html','_blank');
+    
+  }
